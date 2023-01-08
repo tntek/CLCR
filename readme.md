c5991999520d808e9207e7c93741fe463a4951ec
TNTekLab <steventangsong@gmail.com>
2023-01-08 18:30:06 +0800

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,44 @@
+# Official implementation for [DLCR](DLCR: Source Data-free Domain Adaptation via Dynamic Local Context Representation)
+
+
+
+Code (pytorch) for ['DLCR: Source Data-free Domain Adaptation via Dynamic Local Context Representation'] on Office-31, Office-Home, VisDA-C. This article is under review.
+
+### Framework
+
+![](./results/dlcr_framework.jpg)
+
+### Datasets and Prerequisites
+
+You need to download the [Office-31](https://drive.google.com/file/d/0B4IapRTv9pJ1WGZVd1VDMmhwdlE/view), [Office-Home](https://drive.google.com/file/d/0B81rNlvomiwed0V1YUxQdC1uOTg/view), [VisDA-C](https://github.com/VisionLearningGroup/taskcv-2017-public/tree/master/classification) dataset,  modify the path of images in each '.txt' under the folder './data_dlcr/'.
+
+The experiments are conducted on one GPU (NVIDIA RTX TITAN).
+
+- python == 3.7.3
+- pytorch ==1.6.0
+- torchvision == 0.7.0
+
+
+### Training and evaluation
+
+1. Training Source modle. All the settings for different scenarios refers to **./run_source.sh**.
+
+2. Then adapting source model to target domain, with only the unlabeled target data. All the settings for different methods and scenarios refers to **./run_targetr.sh**.
+
+### Results
+
+![](./results/result_oc.jpg)
+
+*The results of DLCR is display under the folder './results/'.*
+
+### Acknowledgement
+
+ [DeepCluster(ECCV 2018)](https://github.com/facebookresearch/deepcluster)'s work.
+
+ [SHOT (ICML 2020, also source-free)](https://github.com/tim-learn/SHOT)'s work.
+
+### Contact
+- tntechlab@hotmail.com
+
+
+
---
+++ run_target.sh
@@ -0,0 +1,28 @@
+# -----------office31----------------
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office --s 0  --t 1
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office --s 0  --t 2
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office --s 1  --t 0
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office --s 1  --t 2
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office --s 2  --t 0
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office --s 2  --t 1
+
+
+# ------------ office-home ----------------
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 0  --t 1
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 0  --t 2
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 0  --t 3
+
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 1  --t 0
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 1  --t 2
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 1  --t 3
+
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 2  --t 0
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 2  --t 1
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 2  --t 3
+
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 3  --t 0
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 3  --t 1
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --gpu_id 0 --dset office-home --s 3  --t 2
+
+# ------------ visda-c----------------
+~/anaconda3/envs/vit_kd/bin/python dlcr_target.py --cls_par 0.2 --da uda --dset VISDA-C --gpu_id 0 --s 0 --t 1 --output_src ckps003du/round01/source_iic/ --output ckps003du/round01/dlcr --net resnet101 --lr 1e-3 --seed 2020
