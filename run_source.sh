aa71e967c2e30f20aa7f2b56528855d5fa631fd2
TNTekLab <steventangsong@gmail.com>
2023-01-28 12:11:52 +0800

    Update run_source.sh

--- run_source.sh
+++ run_source.sh
@@ -1,14 +1,14 @@
 #--------------office-31--source------------------------
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 0
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 1
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 2
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 0
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 1
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office --max_epoch 100 --s 2
 
 
 # -------------office-home--source---------
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 0
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 1
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 2
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 3
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 0
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 1
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 2
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset office-home --max_epoch 50 --s 3
 
 # -------------visda-c--source---------
-~/anaconda3/envs/vit_kd/bin/python dlcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 10 --s 0
+~/anaconda3/envs/vit_kd/bin/python clcr_source_iic.py --trte val --output ckps003du/round01/source_iic/ --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 10 --s 0
